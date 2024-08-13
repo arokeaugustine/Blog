b6ce015ffff9c2b59119ad61f30d67a9f31ad0d6
Augustine A A <Augustine A A@DESKTOP-H4G81TG>
2024-08-13 22:22:24 +0100

    Add project files.

---
+++ Controllers/PostsController.cs
@@ -0,0 +1,62 @@
+﻿using Blog.Models;
+using Blog.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
+using System.Collections.Generic;
+
+namespace Blog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostsController : ControllerBase
+    {
+        private readonly PostsService _postsService;
+        public PostsController()
+        {
+            _postsService = new PostsService();
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<Post>>> GetPost()
+        {
+            var posts = await _postsService.GetAllPosts();
+            return Ok(posts);
+        }
+
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Post>> GetPosts(int id)
+        {
+            var post = await _postsService.GetPost(id);
+            if (post == null)
+            {
+                return NotFound();
+            }
+            return Ok(post);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Post>> CreatePost(Post post)
+        {
+            await _postsService.CreatePost(post);
+            return CreatedAtAction(nameof(GetPost), new { id = post.Id }, post);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> UpdatePost(int id, Post post)
+        {
+            if (id != post.Id)
+            {
+                return BadRequest();
+            }
+            var updatedPost = await _postsService.UpdatePost(id, post);
+            if (updatedPost == null)
+            {
+                return NotFound();
+            }
+            return Ok(post);
+        }
+
+
+    }
+}
---
+++ Services/PostsService.cs
@@ -0,0 +1,48 @@
+﻿using Blog.Models;
+
+namespace Blog.Services
+{
+    public class PostsService
+    {
+        private static readonly List<Post> AllPosts = new ();
+
+        public Task CreatePost(Post item)
+        {
+            AllPosts.Add(item);
+            return Task.CompletedTask;
+        }
+
+        public Task<Post?> UpdatePost(int Id,Post item)
+        {
+            var posts = AllPosts.FirstOrDefault(x => x.Id == Id);
+            if (posts != null)
+            {
+                posts.Title = item.Title;
+                posts.Body = item.Body;
+                posts.UserId = item.UserId;
+            }
+            return Task.FromResult(posts);
+        }
+
+        public Task<Post?> GetPost(int Id)
+        {
+            return Task.FromResult(AllPosts.FirstOrDefault(x => x.Id == Id));
+        }
+        public Task<List<Post>> GetAllPosts()
+        {
+            return Task.FromResult(AllPosts);
+        }
+
+        public Task DeletePost(int Id)
+        {
+            var post = AllPosts.FirstOrDefault(x => x.Id == Id);
+            if (post != null) 
+            { 
+                AllPosts.Remove(post);
+            }
+            return Task.CompletedTask;
+        }
+
+
+    }
+}
